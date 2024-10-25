4a074803875052e4e7bcdfebe251b058bf3ae031
Antônio Carlos Bezerra Cavalcante Junior <carlos.developer@icloud.com>
2024-10-25 17:54:23 -0300

    Correção na connection string

--- CashFlow.Infrastructure/DependencyInjectionExtension.cs
+++ CashFlow.Infrastructure/DependencyInjectionExtension.cs
@@ -22,7 +22,6 @@ namespace CashFlow.Infrastructure
         {
             var connectionString = configuration.GetConnectionString("Connection");
             var serverVersion = new MySqlServerVersion(new Version(8, 0, 40));
-            // optionsBuilder.UseMySql(connectionString, serverVersion);
             services.AddDbContext<CashFlowDbContext>(config => config.UseMySql(connectionString, serverVersion));
         }
     }
