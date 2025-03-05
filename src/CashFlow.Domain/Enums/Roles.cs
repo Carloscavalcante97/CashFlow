e8c5ebf2e5d78b0fbeabf1fc12baa8a14e33e25a
Antônio Carlos Bezerra Cavalcante Junior <126694183+Carloscavalcante97@users.noreply.github.com>
2025-03-04 21:23:54 -0300

    (FEAT): Criação da migrations.

---
+++ src/CashFlow.Domain/Enums/Roles.cs
@@ -0,0 +1,8 @@
+﻿namespace CashFlow.Domain.Enums
+{
+    public class Roles
+    {
+        public const string ADMIN = "Administrator";
+        public const string TEAMMEMBER = "TeamMember";
+    }
+}
